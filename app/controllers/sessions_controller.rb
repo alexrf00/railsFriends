e565ae81238062bf558c16f58ef8acaa3520dd97
Alex <alex.rf.miguel00@gmail.com>
2025-05-31 00:37:22 -0400

    production ready

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,4 @@
 class SessionsController < ApplicationController
-  skip_before_action :authorize_request, only: [:create]
-
   def create
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
